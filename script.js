2ce85bad1169abf0e4dfb334a675ea76dea5e8ca
Donna Lee <dhl287@gmail.com>
2023-07-01 15:15:25 -0700

    removed const characters and added var characters back

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var symbol = "!@#$%^&*()-_=+";
+
+const confirmLength = "";
+
+function generatePassword() {
+  var confirmLength = (prompt("How many charaters would you like to use in your password?"));
+  while (confirmLength <= 8 || confirmLength >= 128) {
+    alert("Your password length needs to be between 8-128 characters. Click 'Generate Password' again.");
+    var confirmLenth = (prompt("How many characters would you like to use in your password?"));
+    alert("Your password will have the correct number of characters.")
+  }
+
+var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters.");
+var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters.");
+var confirmNumber = confirm("Click OK to confirm if you would like to include numbers.");
+var confirmSymbol = confirm("Click OK to confirm if you would like to include symbols.");
+
+while(confirmUpperCase === false && confirmLowerCase === false && confirmNumber === false && confirmSymbol === false) {
+  alert("You must click OK for each type of character to continue.")
+  var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters.");
+  var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters.");
+  var confirmNumber = confirm("Click OK to confirm if you would like to include numbers.");
+  var confirmSymbol = confirm("Click OK to confirm if you would like to include symbols.");
+
+}
+
+  // 1. need to create prompts (prompt the user)
+  //  a. password length (8 - 128 characters)
+  //  b. lowercase, uppercase, number, symbol
+  // 2. validate input, at least one character type is selected
+  // 3. show generated password
+
+  var randomPassword = ""
+      
+  for (var i = 0; i < confirmLength; i++) {
+    randomPassword = randomPassword + characters[Math.floor(Math.random() * characters.length)];
+    console.log(randomPassword)
+  }
+  return randomPassword;
+
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,29 +55,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-function generatePassword(length) {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
-  var password = "";
-
-for (var i = 0; i < length; i++) {
-  const randomIndex = Math.floor(Math.random() * charaters.length);
-  password += characters[randomIndex];
-}
-
-return password;
-}
-
-const password = generatePassword(10);
-console.log(password);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
